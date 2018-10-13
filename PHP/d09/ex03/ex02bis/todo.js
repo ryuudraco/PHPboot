61bb2d3ecf5c1e85eb116f92235ab3289d03a130
Johnathan Dwayne Heath <jheath@student.wethinkcode.co.za>
2018-10-13 13:40:52 +0200

    asdadsa

--- PHP/d09/ex03/ex02bis/todo.js
+++ PHP/d09/ex03/ex02bis/todo.js
@@ -1,47 +1,40 @@
 var ft_list;
 var cookie = [];
 
-window.onload = function () 
-{
-    document.querySelector("#new").addEventListener("click", newTodo);
-    ft_list = document.querySelector("#ft_list");
+$(document).ready(function(){
+    $('#new').click(newTodo);
+    $('#ft_list div').click(deleteTodo);
+    ft_list = $('#ft_list');
     var tmp = document.cookie;
-    if (tmp) 
-    {
+    if (tmp) {
         cookie = JSON.parse(tmp);
-        cookie.forEach(function (e) 
-        {
+        cookie.forEach(function (e) {
             addTodo(e);
         });
     }
-};
+});
 
-window.onunload = function () 
-{
-    var todo = ft_list.children;
+$(window).unload(function(){
+    var todo = ft_list.children();
     var newCookie = [];
     for (var i = 0; i < todo.length; i++)
         newCookie.unshift(todo[i].innerHTML);
     document.cookie = JSON.stringify(newCookie);
-};
+})
 
-function newTodo()
-{
-    var todo = prompt("What would you like to do?", '');
-    if (todo !== '') 
+function newTodo(){
+    var todo = prompt("Que dois-tu faire ?", '');
+    if (todo !== '') {
         addTodo(todo)
+    }
 }
 
-function addTodo(todo)
-{
-    var div = document.createElement("div");
-    div.innerHTML = todo;
-    div.addEventListener("click", deleteTodo);
-    ft_list.insertBefore(div, ft_list.firstChild);
+function addTodo(todo){
+    ft_list.prepend($('<div>' + todo + '</div>').click(deleteTodo));
 }
 
-function deleteTodo()
-{
-    if (confirm("Delete this task?"))
-        this.parentElement.removeChild(this);
+function deleteTodo(){
+    if (confirm("Veux-tu vraiment supprimer cette tâche ?")){
+        this.remove();
+    }
 }
